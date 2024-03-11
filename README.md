e8b64a41a7bf24ae1bf63f062b45ca34e1712897
Aeraglyx <vladislav.cam24@gmail.com>
2024-03-11 01:21:03 +0100

    stuff

--- README.md
+++ README.md
@@ -4,11 +4,15 @@ CLI tool for tuning ATR's *Amps to Accel Ratio* in [Float Pkg](https://github.co
 
 ## Data Acquisition
 
-In VESC Tool, log a ride. Ideally on flat ground, but representable of your normal riding with a variety of accelerations and decelerations. At least stop the recording at the same location or elevation as you started.
+In VESC Tool, log a ride. Ideally on flat ground, but representable of your normal riding with a variety of accelerations and decelerations. If not, at least stop the recording at the same location/elevation as you started, so the ups and downs average out. Few seconds is NOT enough.
 
 ## Installation
 
-Get this repository, make sure you have Python installed, pip install prerequisites:
+- Install [Python](https://www.python.org/downloads/), make sure it's in your path.
+- Get this repository.
+- In your terminal, navigate to the project directory.
+- pip install prerequisites (*scipy*, *matplotlib*):
+ 
 
 ```
 pip install -r requirements.txt
@@ -24,26 +28,28 @@ In your terminal, navigate to the project folder and run:
 python accel_ratio.py -hz 800
 ```
 
-The `-hz`/`--loop_hertz` argument should match your controller's frequency (Float Pkg > Specs > Loop Hertz). Defaults to 800 Hz, which is what the ADV runs at. I believe other common values are 832/833 and 1000 Hz.
+<!-- | Argument | Description |
+| -------- | ----------- |
+| dfa | Description | -->
+
+The `-hz`/`--loop_hertz` argument should match your controller's frequency (Float Pkg > Specs > Loop Hertz). Defaults to 800 Hz, which is what the ADV runs at. Another common value is 832/833 Hz.
 
 Use `-p`/`--plot` flag to plot recorded data along with the predicted curve.
+
+With `-r`/`--ref_ratio` you can specify your current Accel. Ratio for comparison.
 
 TODO image
 
 ## Interpreting the Results
-The predicted Accel Ratio shouldn't be too far from the default 9.0. For a reference, as a lighter rider I got around 8.0.
+Default Accel. Ratio is 9.0, Torque Offset is hard coded to 8 A. The predicted values should be close enough to that. As a rule of thumb, heavier riders should get higher recommended Accel. Ratio, since in the code, higher ratios would lower the expected acceleration.
 
-Torque Offset is hard coded to 8 A, so you can't change it, but again the predicted value should be close enough.
-
-The R Value is indicating how much correlation there is between the measured and predicted acceleration. 1.0 would be perfect correlation, hopefully no less than 0.5.
-
-As a sanity check, here are my values:
-- ADV with v1 motor and bearing covers
+For reference, here are my values:
+- ADV with v1 motor + bearing covers (which add friction)
 - weight: 60kg / 130 lbs
 - suggested Accel Ratio: 7.9
 - suggested Torque Offset: 9.0 A
 
-In the code, higher ratios would lower the expected acceleration, so as a rule of thumb, heavier riders should get higher recommended Accel Ratio.
+Higher correlation means there is stronger relation between the predicted and measured acceleration. 0.0 means completely random, 1.0 would be perfect correlation. You should get no less than 0.5. Recording the log in a more controlled manner should yield in better correlation.
 
 ## Acknowledgements
 
--- accel_ratio.py
+++ accel_ratio.py
@@ -1,117 +1,102 @@
 import argparse
-import csv
 import os
 from math import exp
-from statistics import mean
+from statistics import mean, stdev
 
 import matplotlib.pyplot as plt
 from scipy import stats
 
+import utils
+
 # from scipy.optimize import curve_fit
 
 
-def get_csv_files():
-    directory = os.getcwd()
-    files = os.listdir(directory)
-    csv_files = [file for file in files if file.endswith(".csv")]
-    return csv_files
-
-
-def get_data(csv_file):
-    data = []
-    with open(csv_file, mode="r") as file:
-        csv_reader_dict = csv.DictReader(file, delimiter=";")
-        for row in csv_reader_dict:
-            data.append(row)
-    return data
-
-
-def calculate_accel(data, loop_hertz):
-    erpm = [int(row["erpm"]) for row in data]
-    current_motor = [float(row["current_motor"]) for row in data]
-    ms_today = [int(row["ms_today"]) for row in data]
-
-    accel_measured = []
-    current_measured = []
-    # ratio_list = []
-    for i in range(len(data) - 1):
-        # discard any stationary data points
-        if erpm[i] == 0 or erpm[i + 1] == 0:
-            continue
-
-        time_diff = ms_today[i + 1] - ms_today[i]
-        erpm_diff = erpm[i + 1] - erpm[i]
-        # normalize to erpm change per loop duration
-        erpm_diff *= 1000 / loop_hertz / time_diff
-        current_avg = (current_motor[i + 1] + current_motor[i]) / 2
-
-        accel_measured.append(erpm_diff)
-        current_measured.append(current_avg)
-        # ratio_list.append((current_avg - 8) * time_diff / erpm_diff)
-
-    return accel_measured, current_measured
-
-
-def plot(x, y, ratio_predicted, offset_predicted, num_points):
+def plot_points(x, y, z, num_points):
 
     alpha = exp(-0.0001 * num_points)
-    plt.scatter(x, y, s=20, linewidths=0, color="IndianRed", alpha=alpha)
-    plt.axline(
-        xy1=(0, offset_predicted),
-        slope=ratio_predicted,
-        # color="purple",
-        label=f"Accel. Ratio = {ratio_predicted:.2f}\nTorque Offset = {offset_predicted:.2f} A",
+    plt.scatter(
+        x,
+        y,
+        c=z,
+        cmap="magma",
+        alpha=alpha,
+        s=20,
+        linewidths=0,
     )
 
-    plt.xlabel("Acceleration [ERPM/loop]")
-    plt.ylabel("Motor Current [A]")
+    mean_x = mean(x)
+    mean_y = mean(y)
+    stdev_x = 4 * stdev(x)
+    stdev_y = 4 * stdev(y)
 
-    plt.xlim([-8, 8])
-    plt.ylim([-30, 50])
-
-    plt.legend()
-    plt.show()
+    plt.xlim([mean_x - stdev_x, mean_x + stdev_x])
+    plt.ylim([mean_y - stdev_y, mean_y + stdev_y])
 
 
-def process_csv_file(csv_file, args):
+def plot_line(slope, intercept, color, label):
 
-    data = get_data(csv_file)
-
-    accel_measured, current_measured = calculate_accel(data, args.loop_hertz)
-
-    x = accel_measured
-    y = current_measured
-    num_points = len(x)
-
-    # fit linear function to data
-    slope, intercept, r_value, p_value, std_err = stats.linregress(y, x)
-
-    # inverse the linear function
-    ratio_predicted = 1 / slope
-    offset_predicted = -intercept / slope
-
-    print("")
-    print(f"{csv_file}")
-    print(f"Evaluated {num_points:,} data points")
-    print("")
-    print("RECOMMENDED VALUES:")
-    print(f"Accel. Ratio:  {ratio_predicted:.1f}")
-    print(f"Torque Offset: {offset_predicted:.1f} A")
-    print("")
-    print(f"R Value: {r_value:.2f} (more = better)")
-    # print(f"Std Err: {std_err:.4f}")
-    print("")
-
-    if args.plot:
-        plot(x, y, ratio_predicted, offset_predicted, num_points)
+    plt.axline(
+        xy1=(0, intercept),
+        slope=slope,
+        color=color,
+        linewidth=2.0,
+        label=label,
+    )
 
 
 def main(args):
 
-    csv_files = get_csv_files()
+    csv_files = utils.get_csv_files()
 
-    for csv_file in csv_files:
-        process_csv_file(csv_file, args)
+    # for csv_file in csv_files:
+    #     data = utils.get_data(csv_file)
+    #     process_data(data, args)
+
+    csv_file = csv_files[0]
+    data = utils.get_data(csv_file)
+
+    indices = utils.get_valid_indices(data)
+
+    erpm_diff = utils.get_val_diff(data, "erpm", indices, args.loop_hertz)
+    current_motor = utils.get_val_avg(data, "current_motor", indices)
+
+    # duty_cycle = get_val_avg(data, "duty_cycle", indices)
+    erpm = utils.get_val_avg(data, "erpm", indices)
+    # erpm = [abs(x) for x in erpm]
+
+    x = current_motor
+    y = erpm_diff
+    num_points = len(x)
+
+    # fit linear function to data
+    slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
+
+    ratio_display, offset_display = utils.inverse_lin_func(slope, intercept)
+    spacing = 20
+
+    print("")
+    print(f"{'Log File:':<{spacing}}{os.path.splitext(csv_file)[0]}")
+    print(f"{'Evaluated points:':<{spacing}}{num_points:,}")
+    print("")
+    # print("RECOMMENDED VALUES:")
+    print(f"{'Accel. Ratio:':<{spacing}}{ratio_display:.1f}")
+    print(f"{'Torque Offset:':<{spacing}}{offset_display:.1f} A")
+    print("")
+    print(f"{'Correlation:':<{spacing}}{r_value:.2f}")
+    print("")
+
+    if args.plot:
+        plt.figure(figsize=(8, 8), dpi=100)
+        plot_points(x, y, erpm, num_points)
+        if args.ref_ratio:
+            ratio_ref, offset_ref = utils.inverse_lin_func(args.ref_ratio, 8)
+            plot_line(ratio_ref, offset_ref, (0.4, 0.7, 0.9), "Reference")
+        plot_line(slope, intercept, "purple", "Predicted")
+        plt.xlabel("Motor Current [A]")
+        plt.ylabel("Acceleration [ERPM/loop]")
+
+        plt.legend(loc="lower right")
+        plt.show()
 
 
 if __name__ == "__main__":
@@ -120,6 +105,7 @@ if __name__ == "__main__":
         "-hz",
         "--loop_hertz",
         default=800,
+        metavar="LOOP_HERTZ",
         type=int,
         help="Frequency of the balance loop, should match Float Cfg > Specs > Loop Hertz. For example, ADV uses 800 Hz.",
     )
@@ -127,7 +113,18 @@ if __name__ == "__main__":
         "-p",
         "--plot",
         action="store_true",
-        help="Plot the logged data along with the predicted line.",
+        help="Plot the logged data along with the predicted line using matplotlib.",
+    )
+    parser.add_argument(
+        "-r",
+        "--ref_ratio",
+        # "--hertz",
+        default=9.0,
+        # metavar=("ACCEL", "DECEL"),
+        metavar=("ACCEL"),
+        type=float,
+        nargs=1,
+        help="Your current Accel. Ratio for comparison.",
     )
 
     args = parser.parse_args()
